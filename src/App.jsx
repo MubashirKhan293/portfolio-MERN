12595879dd4ea518b50d9ee9318f3b57662c312c
mubashir <mubikhank30@gmail.com>
2024-10-12 15:03:55 +0500

    add navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Navbar from "./components/Navbar"
 export default function App() {
   return (
-    <Navbar />
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:text-cyan-300">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
+      <div className="container px-8 mx-auto">
+      <Navbar />
+      </div>
+    </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import { FaFacebook } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='flex justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <h3 className='mx-2 w-10'>Mk</h3>
+      </div>
+      <div className='flex gap-4 justify-center items-center m-8 text-2xl'>
+      <FaGithub />
+      <FaLinkedin />
+      <FaTwitterSquare />
+      <FaFacebook />
+      <FaInstagram />
+      </div>
+    </nav>
   )
 }
 
