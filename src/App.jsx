3049bf34e8c17dcce41682d5cf73638d45d34de4
mubashir <mubikhank30@gmail.com>
2024-10-21 20:55:44 +0500

    add projects component done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About"
 import Experience from "./components/Experience"
 import Hero from "./components/hero"
 import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
 import Technologies from "./components/Technologies"
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <About />
       <Technologies />
       <Experience />
+      <Projects />
       </div>
     </div>
   )
