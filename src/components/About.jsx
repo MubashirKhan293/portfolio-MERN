d302838a339726caa2f3e5ce158771947c79b3c3
mubashir <mubikhank30@gmail.com>
2024-10-27 22:18:38 +0500

    add animation to about done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import about from '../assets/about.jpg'
 import { ABOUT_TEXT } from '../constants'
+import { motion } from 'framer-motion'
+
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
       <h1 className='my-20 text-center text-4xl'>About <span className='text-neutral-500'>Me</span></h1>
       <div className='flex flex-wrap'>
-        <div className='w-full lg:w-1/2 lg:p-8'>
+        <motion.div
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity:0, x:-100}}
+        transition={{duration:0.7}}
+         className='w-full lg:w-1/2 lg:p-8'>
           <div className='flex justify-center items-center'>
             <img className='rounded-2xl' src={about} alt="about_img" />
           </div>
-        </div>
+        </motion.div>
         <div className='w-full lg:w-1/2'>
           <div className='flex justify-center lg:justify-start'>
-            <p className='py-6 my-6 max-w-xl'>{ABOUT_TEXT}</p>
+            <motion.p
+             whileInView={{opacity:1, x:0}}
+             initial={{opacity:0, x:100}}
+             transition={{duration:0.7}}
+            className='py-6 my-6 max-w-xl'>{ABOUT_TEXT}</motion.p>
           </div>
         </div>
       </div>
