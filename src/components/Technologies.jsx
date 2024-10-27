fd353024e78fa1cdcff4010a43e7f78ddd98d984
mubashir <mubikhank30@gmail.com>
2024-10-27 22:52:07 +0500

    add animation to technologies done

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,35 +1,84 @@
+import { animate, motion } from 'framer-motion'
 import React from 'react'
 import { FaCss3, FaHtml5, FaNodeJs, FaPhp } from 'react-icons/fa'
 import { RiJavascriptLine, RiReactjsLine, RiTailwindCssLine } from 'react-icons/ri'
 import { SiExpress, SiMongodb, SiMysql } from 'react-icons/si'
 
+const iconVariants=(duration)=>({
+  initial:{y:-10},
+  animate:{
+    y:[10,-10],
+    transition:{
+     duration:duration,
+     ease:"linear",
+     repeat:Infinity,
+     repeatType:'reverse'
+    }
+  }
+})
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-900 pb-24'>
-      <h1 className='my-20 text-4xl text-center'>Technologies</h1>
-      <div className='flex flex-wrap justify-center items-center gap-4'>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+      <motion.h1
+      whileInView={{opacity:1, y:0}}
+      initial={{opacity:0, y:-100}}
+      transition={{duration:1.5}}
+       className='my-20 text-4xl text-center'>Technologies</motion.h1>
+      <motion.div
+       whileInView={{ opacity:1, x:0}}
+       initial={{x:-100, opacity:0}}
+       transition={{duration:1.5}}
+       className='flex flex-wrap justify-center items-center gap-4'>
+        <motion.div
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+         className='rounded-2xl border-4 border-neutral-800 p-4'>
           <SiMongodb className='text-7xl text-green-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(4)}
+         initial="initial"
+         animate="animate"
+         className='rounded-2xl border-4 border-neutral-800 p-4'>
           <SiExpress className='text-7xl'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(6)}
+         initial="initial"
+         animate="animate"
+         className='rounded-2xl border-4 border-neutral-800 p-4'>
           <RiReactjsLine className='text-7xl text-cyan-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div  
+        variants={iconVariants(3.5)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
           <FaNodeJs className='text-7xl text-green-500'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2)}
+         initial="initial"
+         animate="animate"
+         className='rounded-2xl border-4 border-neutral-800 p-4'>
           <RiTailwindCssLine className='text-7xl text-cyan-600'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div  
+        variants={iconVariants(4)}
+        initial="initial"
+        animate="animate"
+        className='rounded-2xl border-4 border-neutral-800 p-4'>
           <FaPhp className='text-7xl text-violet-300'/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        </motion.div>
+        <motion.div 
+         variants={iconVariants(2)}
+         initial="initial"
+         animate="animate"
+         className='rounded-2xl border-4 border-neutral-800 p-4'>
           <SiMysql className='text-7xl'/>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
